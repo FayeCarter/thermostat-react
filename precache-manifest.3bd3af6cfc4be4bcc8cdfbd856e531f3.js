710735490180863a9ed50a185087253daf4eb64a
FayeCarter <fayecarter333@gmail.com>
2020-05-18 09:00:45 +0100

    Updates

---
+++ precache-manifest.3bd3af6cfc4be4bcc8cdfbd856e531f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "088da13f7e8f4de8cd2bd865cce00146",
+    "url": "/thermostat-react/index.html"
+  },
+  {
+    "revision": "a8354e3799d3064932a3",
+    "url": "/thermostat-react/static/css/2.6043711c.chunk.css"
+  },
+  {
+    "revision": "a8354e3799d3064932a3",
+    "url": "/thermostat-react/static/js/2.80ff7958.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/thermostat-react/static/js/2.80ff7958.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c33889f6fdbf684da59",
+    "url": "/thermostat-react/static/js/main.1076e150.chunk.js"
+  },
+  {
+    "revision": "b7e60be34004ed9ad4dd",
+    "url": "/thermostat-react/static/js/runtime-main.e689aef1.js"
+  }
+]);
